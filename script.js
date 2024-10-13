73bce760ed6827e41e796766ec7dfaa6fd77ed65
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2024-10-13 18:01:32 +0200

    Update code

--- script.js
+++ script.js
@@ -2,6 +2,36 @@ const statusEl = document.getElementById('services');
 const lastUpdatedEl = document.getElementById('last-updated');
 
 function updateStatus(data) {
+	data = {
+		"time": "Sun, 13 Oct 2024 17:39:53 CEST",
+		"unixTime": 1728833993504,
+		"services": [
+			{
+				"name": "Cringe Studios Server",
+				"ok": true
+			},
+			{
+				"name": "Cringe Studios Website 2",
+				"ok": true
+			},
+			{
+				"name": "Graphite Server",
+				"ok": true
+			},
+			{
+				"name": "Graphite Website",
+				"ok": true
+			},
+			{
+				"name": "Does not exist",
+				"ok": false
+			},
+			{
+				"name": "Does not exist 2",
+				"ok": false
+			}
+		]
+	};
 	lastUpdatedEl.textContent = new Date(data.time).toLocaleString();
 
 	statusEl.innerHTML = '';
